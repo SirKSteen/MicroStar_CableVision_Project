382e6a89982e8f254fc9a959b0e4d71977d1bcaf
Patrick Forbes <patrickjforbes@students.utech.edu.jm>
2021-03-17 10:12:29 -0500

    Finished Update password view

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -13,21 +13,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
-	LoginForm rs=	new LoginForm();
-	RegistrationForm rf = new RegistrationForm();
-	UpdatePasswordForm up = new UpdatePasswordForm();
-	
-	User user = new User("foo","bar","patri@gmail.com","1234",Role.CUSTOMER);
-	
-	AuthController ac = new AuthController();
-	UserController uc = new UserController();
-	//rs.showForm(Role.CUSTOMER);
-	up.showForm();
-	/*
-	 * try { //uc.createUser(user); ac.login(1711, "1234", Role.CUSTOMER);
-	 * //ac.updatePassword(1711, "secret", "1234"); } catch (CustomizedException e)
-	 * { // TODO Auto-generated catch block e.printStackTrace(); }
-	 */
+
 	
 	
 	}
